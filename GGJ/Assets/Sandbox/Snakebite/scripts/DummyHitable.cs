610dd71c737a4b48c9c3d90e9cfd3d2eb3880703
yoel <syoels@gmail.com>
2020-01-31 03:46:37 +0200

    added sandbox folder and snakebite attack test

---
+++ GGJ/Assets/Sandbox/Snakebite/scripts/DummyHitable.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DummyHitable : MonoBehaviour
+{
+
+    Animator anim;
+    private int ANIM_TRIGGER_BITE;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ANIM_TRIGGER_BITE = Animator.StringToHash("hit");
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Hit() {
+        
+        anim.SetTrigger(ANIM_TRIGGER_BITE);
+    }
+
+}
