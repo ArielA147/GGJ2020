4dacaf6423a0c3a9bf49a049c1bc3ee59301b34d
Ariel Alexi <ariel.147@gmail.com>
2020-01-31 02:35:31 +0200

    creating base RobotPartBase

---
+++ GGJ/Assets/Scripts/RobotPartBase.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class RobotPartBase : MonoBehaviour
+{
+    private int lives;
+    public int Lives {
+        get{return lives;}
+    }
+
+    void PrintRemainingLives(){
+        Console.WriteLine(lives);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
