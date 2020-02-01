dce3086664cd54fc8389fbaf96c9622fad08517a
yagelch <jogichernia@gmail.com>
2020-02-01 10:55:47 +0200

    cam  comment ed out
    cam  comment ed out

--- GGJ/Assets/Scripts/Main_Camera.cs
+++ GGJ/Assets/Scripts/Main_Camera.cs
@@ -4,17 +4,33 @@ using UnityEngine;
 
 public class Main_Camera : MonoBehaviour
 {
-    private Player [] players;
+    public Camera cam;
+    public Player [] players;
+    float curr_size;
     // Start is called before the first frame update
     void Start()
     {
         GameObject.FindObjectsOfType<Player>();
-        players = new Player[2];
+        
     }
 
     // Update is called once per frame
     void Update()
-    {
-        
+    {/*
+        foreach (Player player in players)
+        {
+            if ( cam.WorldToScreenPoint( player.transform.position ).y > Screen.height && 
+                Mathf.Abs((players[0].transform.position.y - players[1].transform.position.y) ) < Screen.height)
+            {
+                Debug.Log("ttt");
+                curr_size = cam.orthographicSize;
+                cam.orthographicSize += 0.1f;
+            }
+            else if (player.transform.GetComponent<Rigidbody2D>().velocity.y < 0 && cam.WorldToScreenPoint(player.transform.position).y < Screen.height/2f )
+            {
+                //cam.orthographicSize -= 0.1f;
+            }
+        } */
     }
+   
 }
