c3a0036f5802c20abe74309ac754ec8911bf314f
yoel <syoels@gmail.com>
2020-02-01 05:43:26 +0200

    added shake to robot chunks

--- GGJ/Assets/Scripts/RobotChunk.cs
+++ GGJ/Assets/Scripts/RobotChunk.cs
@@ -6,16 +6,27 @@ public class RobotChunk : MonoBehaviour
 {
 
     Robot parentRobot;
+    Vector3 _startPosition;
+    float shakeAmount = 0.075f;
+    float shakeSpeed = 15f;
+    float offset;
+    public bool isBottomChumk = false;
+
 
     // Start is called before the first frame update
     void Start()
     {
         parentRobot = GetComponentInParent<Robot>();
+        offset = Random.Range(0f, Mathf.PI);
+        _startPosition = transform.localPosition;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!isBottomChumk)
+        {
+            transform.localPosition = _startPosition + new Vector3(0.0f, Mathf.Sin((offset + Time.time) * shakeSpeed) * shakeAmount, 0.0f);
+        }
     }
 }
